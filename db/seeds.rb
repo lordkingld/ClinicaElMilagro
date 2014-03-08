26ca81fee294eddb208d0ef951cd2ccc47e94ce2
Francisco Quintero <frajaquico@gmail.com>
2014-03-08 13:15:22 -0500

    agregar seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,18 @@
 
 user = User.create!({:email => "jloviedos@clinicaelmilagro.com", :role_user => "Admin", :password => "tacos123.", :password_confirmation => "tacos123." })
 Person.create( :user_id => user.id , :name => 'Jose' , :last_name => 'Oviedo',:sex=>'Male', :birthday=>'19-04-1988',:cell_number=>'3004273468',:city=>'Barranquilla',:address=>'Calle 45B # 8A - 33',:status=>'Activo',:type_person=>'Admin')
+
+user = User.create!({:email => "ldaza@clinicaelmilagro.com", :role_user => "Patient", :password => "tacos123.", :password_confirmation => "tacos123." })
+Person.create( :user_id => user.id , :name => 'Luis' , :last_name => 'Daza',:sex=>'Male', :birthday=>'15-04-2001',:cell_number=>'89028271',:city=>'Barranquilla',:address=>'Calle 78 # 8A - 33',:status=>'Activo',:type_person=>'Patient')
+
+user = User.create!({:email => "corozco@clinicaelmilagro.com", :role_user => "Patient", :password => "tacos123.", :password_confirmation => "tacos123." })
+Person.create( :user_id => user.id , :name => 'Cesar' , :last_name => 'Orozco',:sex=>'Male', :birthday=>'19-04-1988',:cell_number=>'3004273468',:city=>'Barranquilla',:address=>'Calle 45B # 8A - 33',:status=>'Activo',:type_person=>'Patient')
+
+user = User.create!({:email => "lgonzales@clinicaelmilagro.com", :role_user => "Patient", :password => "tacos123.", :password_confirmation => "tacos123." })
+Person.create( :user_id => user.id , :name => 'Luis' , :last_name => 'Gonzales',:sex=>'Male', :birthday=>'19-04-1988',:cell_number=>'3004273468',:city=>'Barranquilla',:address=>'Calle 45B # 8A - 33',:status=>'Activo',:type_person=>'Patient')
+
+user = User.create!({:email => "cpuello@clinicaelmilagro.com", :role_user => "Doctor", :password => "tacos123.", :password_confirmation => "tacos123." })
+Person.create( :user_id => user.id , :name => 'Carlos' , :last_name => 'Puello',:sex=>'Male', :birthday=>'19-04-1988',:cell_number=>'3004273468',:city=>'Barranquilla',:address=>'Calle 45B # 8A - 33',:status=>'Activo',:type_person=>'Doctor')
+
+user = User.create!({:email => "torozco@clinicaelmilagro.com", :role_user => "Doctor", :password => "tacos123.", :password_confirmation => "tacos123." })
+Person.create( :user_id => user.id , :name => 'Tulio' , :last_name => 'Orozco',:sex=>'Male', :birthday=>'19-04-1988',:cell_number=>'3004273468',:city=>'Barranquilla',:address=>'Calle 45B # 8A - 33',:status=>'Activo',:type_person=>'Doctor')
